31d78c684e9c2ee7c2114761014df4b19b80e878
SamuelWas <samuelwillianalmeida@gmail.com>
2019-10-13 14:45:31 -0300

    Fixed startIndex

--- somamax.c
+++ somamax.c
@@ -10,21 +10,23 @@ int main()
         scanf("%d", &val[i]);
     }
 
-    int maxVectorSum = 0, currentVectorSum = 0, startIndex = 0, endIndex = 0;
+    int maxVectorSum = 0, currentVectorSum = 0, possibleStartIndex, startIndex, endIndex;
     for(int i = 0; i < n; ++i) {
         if(currentVectorSum > 0) {
             currentVectorSum = val[i] + currentVectorSum;
         } else {
             currentVectorSum = val[i];
-            startIndex = i + 1;
+            possibleStartIndex = i + 1;
         }
         if(currentVectorSum > maxVectorSum) {
+            startIndex = possibleStartIndex;
             endIndex = i + 1;
             maxVectorSum = currentVectorSum;
         }
     }
 
     printf("Soma: %d\n", maxVectorSum);
-    printf("Indices: %d a %d", startIndex, endIndex);
+    printf("Indices: %d a %d\n", startIndex, endIndex);
+    system("pause");
     return 0;
 }
